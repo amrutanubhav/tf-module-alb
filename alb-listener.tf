d4a9d0fcb5dc306309b3bb8e7f8990617befa6d0
Amrut Anubhav <anubhavamrut@gmail.com>
2023-04-23 12:45:53 +0530

    updating

---
+++ alb-listener.tf
@@ -0,0 +1,21 @@
+#create listener for alb and add the required rules\
+#listener rules should come from appropriate components
+
+resource "aws_lb_listener" "private" {
+  count             = var.INTERNAL ? 1 : 0
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "I am operational"
+      status_code  = "200"
+    }
+  }
+
+}
+
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "ALB_ARN" {
     value = aws_lb.alb.arn
 }
+
+output "LISTENER_ARN" {
+    value = aws_lb_listener.private.*.arn
+}
+
